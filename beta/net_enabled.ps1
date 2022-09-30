a2f973c726db5abaaf11cb3c2fed704da4666237
Lekszikov Miklós <lekszikov.guru@gmail.com>
2022-09-30 19:02:48 +0200

    Beta !!! Added Net-Disabled Added Net-Enabled Added HOSTS file edit

---
+++ beta/net_enabled.ps1
@@ -0,0 +1,8 @@
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
+
+netsh interface set interface "Ethernet" enabled
--- modul/my_script.py
+++ modul/my_script.py
@@ -1,23 +1,35 @@
-'''import os
-import subprocess
-from subprocess import Popen, CREATE_NEW_CONSOLE'''
-
 import os
 import lang
 import logo
 import menu_list_def
+from subprocess import Popen, CREATE_NEW_CONSOLE
+
+ROOT_DIR = os.path.abspath(os.curdir)
+net_disabled = ROOT_DIR + "\\beta\\net_disabled.ps1"
+net_enabled = ROOT_DIR + "\\beta\\net_enabled.ps1"
+host_edit = ROOT_DIR + "\\beta\\hosts_edit.ps1"
+
 
 def beta_my_script():
     while True:
         menu_list_def.clear()
         logo.logos.main_logo()
         print("beta project ")
+        print("[ 0]: Ethernet Disabled")
+        print("[ 1]: Ethernet Enabled")
+        print("[ 2]: HOSTS EDIT!!!")
         menu_list_def.exits_text()
 
         system_lista = int(input("" + lang.language.langs["main"][6]))
 
         if system_lista == 0:
-            pass
+            Popen('powershell ' + net_disabled, creationflags=CREATE_NEW_CONSOLE)
+
+        if system_lista == 1:
+            Popen('powershell ' + net_enabled, creationflags=CREATE_NEW_CONSOLE)
+
+        if system_lista == 2:
+            Popen('powershell ' + host_edit, creationflags=CREATE_NEW_CONSOLE)
 
         if system_lista == 20:
             break
