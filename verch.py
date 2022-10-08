f6fd7d6c484ea1f5307f67b3fc466e301ff602c6
Lekszikov Miklós <lekszikov.guru@gmail.com>
2022-10-08 16:27:44 +0200

    Fixed_list
    Fixed Verzion

--- verch.py
+++ verch.py
@@ -1,4 +1,8 @@
 import requests
+import os
+import json
+
+ROOT_DIR = os.path.abspath(os.curdir)
 
 class ANSI():
   def background(code):
@@ -10,11 +14,18 @@ class ANSI():
   def color_text(code):
     return "\33[{code}m".format(code=code)
 
-url = 'https://raw.githubusercontent.com/LexyGuru/Terminal_Windows_URI_PY/main/SVG_DIR/verzion.json'
-x = requests.get(url)
-current = x.json()['current'][0]
 
-url = 'https://raw.githubusercontent.com/LexyGuru/Terminal_Windows_URI_PY/beta/SVG_DIR/verzion.json'
+'''url = 'https://raw.githubusercontent.com/LexyGuru/Terminal_Windows_URI_PY/beta/SVG_DIR/verzion.json'
+x = requests.get(url)
+beta_ver = x.json()['current'][0]'''
+
+
+with open(ROOT_DIR + '\\lang\\version.json', "r") as file:
+  jsonData = json.load(file)
+current = jsonData['current'][0]
+
+
+url = 'https://raw.githubusercontent.com/LexyGuru/Terminal_Windows_URI_PY/main/SVG_DIR/verzion.json'
 x = requests.get(url)
 new_ver = x.json()['current'][0]
 
@@ -23,7 +34,7 @@ def ver_ch():
   b = new_ver
 
   if b > a:
-    example_ansi = ANSI.background(0) + ANSI.color_text(39) + ANSI.style_text(31) + "New Beta ver: " + b
+    example_ansi = ANSI.background(0) + ANSI.color_text(39) + ANSI.style_text(31) + "New ver: " + b
     print(example_ansi)
 
   elif a == b:
