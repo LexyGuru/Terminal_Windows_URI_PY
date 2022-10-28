196fae54836bdf01c9f7eea09991bea9cfd25547
Lekszikov Miklós <lekszikov.guru@gmail.com>
2022-10-28 16:21:52 +0200

    1.2.55v Beta
    Fixed verch

--- verch.py
+++ verch.py
@@ -1,6 +1,6 @@
 import requests
 import os
-#import json
+import json
 from sty import fg, ef, rs
 import lang.language
 
@@ -9,17 +9,18 @@ ROOT_DIR = os.path.abspath(os.curdir)
 
 class verzion():
     def ver_ch(self):
-        '''with open(ROOT_DIR + '\\version_current.json', "r") as file:
+        '''with open(ROOT_DIR + '\\SVG_DIR\\version_current.json', "r") as file:
             jsonData = json.load(file)
             current = jsonData['current'][0]'''
-
-        url = 'https://github.com/LexyGuru/Terminal_Windows_URI_PY/tree/beta/SVG_DIR/verzion_current.json'
-        x = requests.get(url)
-        current = x.json()['current'][0]
 
         url = 'https://raw.githubusercontent.com/LexyGuru/Terminal_Windows_URI_PY/main/SVG_DIR/verzion.json'
         x = requests.get(url)
         new_ver = x.json()['current'][0]
+
+        #url = 'https://raw.githubusercontent.com/LexyGuru/Terminal_Windows_URI_PY/main/SVG_DIR/version_current.json'
+        url = 'https://raw.githubusercontent.com/LexyGuru/Terminal_Windows_URI_PY/main/SVG_DIR/verzion.json'
+        x = requests.get(url)
+        current = x.json()['next_current'][0]
 
         a = current
         b = new_ver
@@ -38,13 +39,9 @@ class verzion():
         x = requests.get(url)
         beta_ver = x.json()['current'][0]
 
-        url = 'https://github.com/LexyGuru/Terminal_Windows_URI_PY/tree/beta/SVG_DIR/verzion_current.json'
-        x = requests.get(url)
-        current = x.json()['current'][0]
-
-        '''with open(ROOT_DIR + '\\version_current.json', "r") as file:
+        with open(ROOT_DIR + '\\SVG_DIR\\version_current.json', "r") as file:
             jsonData = json.load(file)
-            current = jsonData['current'][0]'''
+            current = jsonData['current'][0]
 
         a = current
         b = beta_ver
